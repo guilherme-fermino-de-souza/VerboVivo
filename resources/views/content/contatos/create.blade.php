0061c999ef1c4bc691bf11e05848fef04ff2b726
GustaFer23 <gustafermino@gmail.com>
2025-09-30 19:33:59 -0300

    Contato terminado, criar livro não tenho o que mexer (ou mudo literalmente todos os outros forms por padronização)

--- resources/views/content/contatos/create.blade.php
+++ resources/views/content/contatos/create.blade.php
@@ -1,4 +1,13 @@
 <div class="container-create">
+
+    <div class="container-create-mapa">
+        <div class="info-mapa-contato">
+            <h1>Nossa Sede</h1>
+            <iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.431335149371!2d-46.40218112593123!3d-23.552947178805574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce65086cafaf55%3A0xf7da96815e7611da!2sEscola%20T%C3%A9cnica%20Estadual%20de%20Guaianazes!5e0!3m2!1spt-BR!2sbr!4v1758999037621!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+
+
     <div class="container-create-conteudo">
         <h1>Entre em Contato</h1>
         <form action="{{route('contato.store')}}" method="post">
@@ -6,13 +15,13 @@
             <input type="hidden" name="name" value="{{ Auth::user()->name }}" required>
             @csrf
             <div class="textfield">
-                <input type="text" id="assunto" name="assunto" required>
+                <input type="text" id="assunto" name="assunto" required maxlength="70">
                 <label for="assunto">Assunto da mensagem</label>
             </div>
 
             <div class="textfield">
-                <textarea id="texto" name="texto" required maxlength="500"></textarea>
-                <label for="texto">Digite sua mensagem</label>
+                <textarea id="texto" name="texto" required maxlength="700"></textarea>
+                <label for="texto">Mensagem</label>
             </div>
 
 
