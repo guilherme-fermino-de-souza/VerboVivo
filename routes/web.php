13bffebbb35d7ccc3cc8035fc4ab45dae9d2a38a
GustaFer23 <gustafermino@gmail.com>
2025-10-01 11:28:08 -0300

    tirei dashboard das rotas

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::resource("/consumidor", ConsumidorController::class)->names("consumidor");
 
 Route::get('/', function () {
